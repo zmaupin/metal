7e754837a891bc750039add8848efd9e63eae8be
Demitri Swan <demitriswan@gmail.com>
2019-02-23 15:22:32 -0800

    preliminary output streaming support

--- rexecd/api/router.go
+++ rexecd/api/router.go
@@ -9,19 +9,19 @@ import (
 	"github.com/metal-go/metal/config"
 )
 
-func newRouter() *mux.Router {
+func newRouter(timeout time.Duration) *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
-	router.HandleFunc("/streamoutput", serveStreamOutputClient)
+	router.HandleFunc("/streamoutput", func(w http.ResponseWriter, r *http.Request) { serveStreamOutputClient(timeout, w, r) })
 	return router
 }
 
-func run() error {
+func run(timeout time.Duration) error {
 	s := &http.Server{
 		Addr:         config.RexecdGlobal.APIAddress,
-		Handler:      newRouter(),
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
+		Handler:      newRouter(timeout),
+		ReadTimeout:  timeout,
+		WriteTimeout: timeout,
 	}
 	return s.ListenAndServe()
 }
