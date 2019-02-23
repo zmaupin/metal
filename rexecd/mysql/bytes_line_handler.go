0a092ac533a662157928dd9fe4aa759a7ce5e95b
Demitri Swan <demitriswan@gmail.com>
2019-02-23 08:11:28 -0800

    notifying no more message to be sent on kafka topic

--- rexecd/mysql/bytes_line_handler.go
+++ rexecd/mysql/bytes_line_handler.go
@@ -27,8 +27,6 @@ type BytesLineHandler struct {
 	kafkaAsyncProducer sarama.AsyncProducer
 	handlerType        BytesLineHandlerType
 	lineNo             uint64
-	stdout             []byte
-	stderr             []byte
 	topic              string
 	init               bool
 }
@@ -40,8 +38,6 @@ func NewBytesLineHandler(command *Command, handlerType BytesLineHandlerType, kaf
 		handlerType:        handlerType,
 		kafkaAsyncProducer: kafkaAsyncProducer,
 		kafkaClusterAdmin:  kafkaClusterAdmin,
-		stdout:             []byte{},
-		stderr:             []byte{},
 	}
 }
 
@@ -53,11 +49,8 @@ func (b *BytesLineHandler) Handle(ctx context.Context, data []byte) error {
 		}
 	}
 	b.lineNo++
-	buf := &bytes.Buffer{}
-	if err := binary.Write(buf, binary.LittleEndian, b.lineNo); err != nil {
-		return err
-	}
-	key, err := ioutil.ReadAll(buf)
+
+	key, err := b.uint64ToKey(b.lineNo)
 	if err != nil {
 		return err
 	}
@@ -72,7 +65,25 @@ func (b *BytesLineHandler) Handle(ctx context.Context, data []byte) error {
 
 // Finish wraps up the handling of bytes
 func (b *BytesLineHandler) Finish(ctx context.Context) error {
+	key, err := b.uint64ToKey(0)
+	if err != nil {
+		return err
+	}
+	b.kafkaAsyncProducer.Input() <- &sarama.ProducerMessage{
+		Key:   sarama.ByteEncoder(key),
+		Topic: b.topic,
+		Value: sarama.ByteEncoder([]byte("EOF")),
+	}
 	return nil
+}
+
+func (b *BytesLineHandler) uint64ToKey(i uint64) ([]byte, error) {
+	buf := &bytes.Buffer{}
+	if err := binary.Write(buf, binary.LittleEndian, b.lineNo); err != nil {
+		return nil, err
+	}
+	key, err := ioutil.ReadAll(buf)
+	return key, err
 }
 
 func (b *BytesLineHandler) initFunc() error {
